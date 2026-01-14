959d719c5f21d18434498d85c2910f89c0006a19
hankyeol kim <r93211@gmail.com>
2026-01-14 10:18:38 +0900

    2026.01.14

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -52,7 +52,7 @@ function Main() {
                     }}
                 >
                     <iframe
-                        src="http://3.36.174.240/index.htm"
+                        src="http://3.36.174.240:8080/index.htm"
                         style={{
                             width: '100%',
                             height: '100%',
